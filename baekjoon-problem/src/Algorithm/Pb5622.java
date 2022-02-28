eae99d164ad504691a0dddd3363a66f1898a4bef
Sua Hwang <powesim@naver.com>
2022-02-28 21:08:22 +0900

    Pb5622 : dial

---
+++ baekjoon-problem/src/Algorithm/Pb5622.java
@@ -0,0 +1,51 @@
+package Algorithm;
+
+import java.io.IOException;
+
+public class Pb5622 {
+	public static void main(String[] args) throws IOException{
+		int ch, time=0;
+		while(true) {
+			ch = System.in.read();
+			if(ch == 10) break;
+			
+			switch ((char)ch) {
+			case 'A': case 'B': case 'C': {
+				time+=3;
+				break;
+			}
+			case 'D': case 'E': case 'F': {
+				time+=4;
+				break;
+			}
+			case 'G': case 'H': case 'I': {
+				time+=5;
+				break;
+			}
+			case 'J': case 'K': case 'L': {
+				time+=6;
+				break;
+			}
+			case 'M': case 'N': case 'O': {
+				time+=7;
+				break;
+			}
+			case 'P': case 'Q': case 'R': case 'S':{
+				time+=8;
+				break;
+			}
+			case 'T': case 'U': case 'V': {
+				time+=9;
+				break;
+			}
+			case 'W': case 'X': case 'Y': case 'Z':{
+				time+=10;
+				break;
+			}
+			
+		}
+			
+		}
+		System.out.print(time);
+	}
+}
